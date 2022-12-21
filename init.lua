833cb06a86bac0cc9afa71b168281ede41d42028
asep komarudin <pojokcodeid@gmail.com>
2022-12-21 19:18:40 +0700

    update

--- init.lua
+++ init.lua
@@ -575,13 +575,13 @@ local config = {
     --     ["~/%.config/foo/.*"] = "fooscript",
     --   },
     -- }
-    vim.api.nvim_create_autocmd("Filetype", {
-      pattern = "java", -- autocmd to start jdtls
-      callback = function()
-        local config = astronvim.lsp.server_settings "jdtls"
-        if config.root_dir and config.root_dir ~= "" then require("jdtls").start_or_attach(config) end
-      end,
-    })
+    -- vim.api.nvim_create_autocmd("Filetype", {
+    --   pattern = "java", -- autocmd to start jdtls
+    --   callback = function()
+    --     local config = astronvim.lsp.server_settings "jdtls"
+    --     if config.root_dir and config.root_dir ~= "" then require("jdtls").start_or_attach(config) end
+    --   end,
+    -- })
   end,
 }
 
